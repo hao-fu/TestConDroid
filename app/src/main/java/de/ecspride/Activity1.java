e9bdc2ba11f06a238fe17f68fc53a72680947ee9
Hao Fu <jamesfuhao@gmail.com>
2016-01-05 15:37:12 -0800

    add ctx factors

--- app/src/main/java/de/ecspride/Activity1.java
+++ app/src/main/java/de/ecspride/Activity1.java
@@ -6,26 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.Calendar;
+
 /**
- * @testcase_name Activity1
- * @version 0.2
  * @author Hao
+ * @version 0.2
+ * @testcase_name Activity1
  * @modified_from DroidBench
  * @author_mail jamesfuhao@gmail.com
- * 
  * @description The sink is called after the user clicks a button. The button
- *  handler is defined via XML.
- *  Add a wrapper to sendMessage
- * @dataflow  source: button1, button2 retrive id; sink: intent;
+ * handler is defined via XML.
+ * Add a wrapper to sendMessage
+ * @dataflow source: button1, button2 retrive id; sink: intent;
  * @number_of_leaks 1
  * @challenges the analysis must analyze the layout xml file and take the lifecycle into account (onCreate is executed before user interaction)
  */
 public class Activity1 extends Activity {
-	private static String imei = null;
-	
+    private static String imei = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +52,27 @@ public class Activity1 extends Activity {
     }
 
     public void getDevIdWrapperTop() {
+        long[] res = getNaturalEvnCtxs();
+        long time = res[0];
+        long seconds = res[1];
+        if (time > seconds) {
+            Log.i("0", "1" + time + seconds);
+            while (time > 2 && seconds > 3) {
+                Log.i("2", "3" + time + seconds);
+                // getDevIdWrapper2();
+            }
+        }
+
         getDevIdWrapper2();
+    }
+
+    private long[] getNaturalEvnCtxs() {
+        long[] result = new long[2];
+        result[0] = System.currentTimeMillis();
+        Calendar calendar = Calendar.getInstance();
+        result[1] = calendar.get(Calendar.SECOND);
+
+        return result;
     }
 
     public void getDevIdWrapper2() {
