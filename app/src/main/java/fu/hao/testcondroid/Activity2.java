6b9a86ae993567d169a9c63be37cc4c4d2e56848
Hao Fu <haofu@ucdavis.edu>
2016-11-14 18:34:32 -0800

    Add an Ad module

--- app/src/main/java/fu/hao/testcondroid/Activity2.java
+++ app/src/main/java/fu/hao/testcondroid/Activity2.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.mopub.mobileads.MoPubErrorCode;
+import com.mopub.mobileads.MoPubView;
 
-public class Activity2 extends Activity {
+
+public class Activity2 extends Activity implements MoPubView.BannerAdListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,43 @@ public class Activity2 extends Activity {
                 sendMsgWrapper2();
             }
         });
+
+        moPubView = (MoPubView) findViewById(R.id.adview);
+        moPubView.setAdUnitId("bcaebdd508184f518e78c8e2797102c1");
+        moPubView.loadAd();
+        moPubView.setBannerAdListener(this);
+
+    }
+
+    @Override
+    public void onBannerLoaded(MoPubView banner) {
+        Toast.makeText(getApplicationContext(),
+                "Banner successfully loaded.", Toast.LENGTH_SHORT).show();
+    }
+
+
+    // Sent when the banner has failed to retrieve an ad. You can use the MoPubErrorCode value to diagnose the cause of failure.
+    public void onBannerFailed(MoPubView banner, MoPubErrorCode errorCode) {
+        Toast.makeText(getApplicationContext(),
+                "Banner successfully loaded.", Toast.LENGTH_SHORT).show();
+    }
+
+    // Sent when the user has tapped on the banner.
+    public void onBannerClicked(MoPubView banner) {
+        Toast.makeText(getApplicationContext(),
+                "Banner successfully loaded.", Toast.LENGTH_SHORT).show();
+    }
+
+    // Sent when the banner has just taken over the screen.
+    public void onBannerExpanded(MoPubView banner) {
+        Toast.makeText(getApplicationContext(),
+                "Banner successfully loaded.", Toast.LENGTH_SHORT).show();
+    }
+
+    // Sent when an expanded banner has collapsed back to its original size.
+    public void onBannerCollapsed(MoPubView banner) {
+        Toast.makeText(getApplicationContext(),
+                "Banner successfully loaded.", Toast.LENGTH_SHORT).show();
     }
 
     public void sendMsgByView(View view) {
@@ -49,4 +89,15 @@ public class Activity2 extends Activity {
     private void sendMsgWrapper2() {
         sendMessage();
     }
+
+    // Declare an instance variable for your MoPubView.
+    private MoPubView moPubView;
+
+
+    protected void onDestroy() {
+        moPubView.destroy();
+        super.onDestroy();
+    }
+
+
 }
